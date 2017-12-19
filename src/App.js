c66614a20315108020b1363ec3e998c4949701b8
Cherie Lum <charismatic.cherie@gmail.com>
2017-12-19 18:31:45 -0500

    adding stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component } from 'react'; 
+import './App.css';
+import MemoryGame from './MemoryGame.js'
+
+class App extends Component {
+    render() {
+        return <MemoryGame />
+    }
+}
+
+export default App; 
+
+
+
